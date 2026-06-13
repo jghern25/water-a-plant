f91df05930eac399fb5846f2e0c73260a5a00a62
jghern25 <jghern25@asu.edu>
2026-06-12 20:50:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+let score = 0;
 
+const button = document.getElementById("waterBtn");
+const plant = document.getElementById("plant");
+const scoreDisplay = document.getElementById("score");
+
+button.addEventListener("click", () => {
+    score++;
+    scoreDisplay.textContent = score;
+
+    if (score >= 5) {
+        plant.textContent = "🌿";
+    }
+
+    if (score >= 10) {
+        plant.textContent = "🌸";
+    }
+});
